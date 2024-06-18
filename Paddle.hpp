dc686b655ddb4b45b58fd1f6b8debfa3fbb82b3e
Aarav D.C <dc.aarav@gmail.com>
2024-06-18 09:41:36 +0545

    Created the class for Paddle

---
+++ Paddle.hpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class Paddle{
+    private:
+    int x,y;
+    int originalX,originalY;
+    public:
+    Paddle(){
+        x=0;
+        y=0;
+    }
+    Paddle(int posx,int posy):Paddle(){
+        originalX = posx;
+        originalY = posy;
+        x = posx;
+        y = posy;
+    }
+    inline void reset(){
+        x = originalX;
+        y = originalY;
+    }
+    inline int getX(){
+        return x;
+    }
+    inline int getY(){
+        return y;
+    }
+    inline void moveUp(){
+        y++;
+    }
+    inline void moveDown(){
+        y--;
+    }
+    friend ostream & operator<<(ostream &ofs,Paddle p);
+};
+ostream & operator<<(ostream &ofs,Paddle p){
+    o << "Paddle ["<< c.x <<","<<c.y<<"]";
+    return o;
+}
---
+++ gameManager.hpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include"Ball.hpp"
+#include"Paddle.hpp"
+using namespace std;
+class gameManager{
+    
+}
